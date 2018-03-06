ced5294c9feebd708f7030b40207a90a56f25ff1
Antoine <antoine1.levy@epitech.eu>
2018-03-06 10:29:47 +0100

    premier push let's go

---
+++ include/philo.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** philosopher
+** File description:
+** header - philo
+*/
+
+#ifndef PHILO_H_
+# define PHILO_H_
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+
+
+#endif /* PHILO_H_ */
---
+++ src/main.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** philosopher
+** File description:
+** corefile - main
+*/
+
+# include "extern.h"
+# include "philo.h"
+
+
