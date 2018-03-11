cf15bde3a7130d1758377d5073a23bf5173e6acd
Antoine <antoine1.levy@epitech.eu>
2018-03-11 17:17:59 +0100

    ajout du --help

--- sources/main.c
+++ sources/main.c
@@ -43,13 +43,31 @@ int	get_eat(int ac, char **av)
 	return (-1);
 }
 
+int	get_help(int ac, char **av)
+{
+	int	i = 0;
+
+	while (i != ac) {
+		if (strcmp(av[i], "--help") == 0) {
+			printf("USAGE\n");
+			printf("       ./philo -p nbr_p -e nbr_e\n\n");
+			printf("DESCRIPTION\n");
+			printf("       -p nbr_p number of philosophers\n");
+			printf("       -e nbr_e maximum times a philosopher ");
+			printf("eats before exiting the program\n");
+		}
+		i++;
+	}
+	return (-1);
+}
+
 int	main(int ac, char **av)
 {
 	int	nb_phil = get_phil(ac, av);
 	int	nb_eat = get_eat(ac, av);
 
 	if (nb_phil <= 0 || nb_eat < 0) {
-		printf("Wrong parameters\n");
+		get_help(ac, av);
 		return (84);
 	}
 	RCFStartup(ac, av);
